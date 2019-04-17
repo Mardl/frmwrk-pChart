877c7d6516e19ea9ba42964a0b5f37a8cbb6a498
Martin Eisenführer <martin@eisenfuehrer.de>
2019-04-17 15:58:36 +0200

    Fix initialisierung in pData

--- src/class/pData.class.php
+++ src/class/pData.class.php
@@ -775,7 +775,7 @@
 
    /* Convert a string to a single elements array */
    function convertToArray($Value)
-    { $Values = ""; $Values[] = $Value; return($Values); }
+    { $Values = []; $Values[] = $Value; return($Values); }
 
    /* Class string wrapper */
    function __toString()
